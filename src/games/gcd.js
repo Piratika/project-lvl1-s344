c6476acb063ca4ef4b1a916b17249577dd48b6fb
Piratika <pira112120@gmail.com>
2018-09-12 19:11:11 +0300

    fix mentor comment

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,11 +5,10 @@ import game from '..';
 const rules = 'Find the greatest common divisor of given numbers.';
 
 const findfcd = (number1, number2) => {
-  while (number1 !== 0 && number2 !== 0) {
-    if (number1 > number2) {
-      return findfcd(number2, number1 % number2);
-    } return findfcd(number1, number2 % number1);
-  } return number1 + number2;
+  if (number1 === 0 || number2 === 0) return number1 + number2;
+  if (number1 > number2) {
+    return findfcd(number2, number1 % number2);
+  } return findfcd(number1, number2 % number1);
 };
 
 const question = () => {
