9cce6ba5c27d2c44f983cd10f8d4c0e7ad81eeac
Piratika <pira112120@gmail.com>
2018-09-12 11:23:44 +0300

    add calc game

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -4,16 +4,12 @@ import { cons } from 'hexlet-pairs';
 
 const rules = 'Answer "yes" if number even otherwise answer "no".';
 
-const generateRandomNumber = () => {
-  const min = 0;
-  const max = 500;
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const isEven = number => number % 2 === 0;
 
 const taskAndAnswer = () => {
-  const randomNumber = generateRandomNumber();
+  const randomNumber = generateRandomNumber(0, 500);
   const rightAnswer = isEven(randomNumber) ? 'yes' : 'no';
   return cons(randomNumber, rightAnswer);
 };
