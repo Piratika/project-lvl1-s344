445ac0e5569a850eb27fb7eb2617b5ad95626798
Piratika <pira112120@gmail.com>
2018-09-12 15:18:58 +0300

    add gcd game

---
+++ src/games/gcd.js
@@ -0,0 +1,21 @@
+import { cons } from 'hexlet-pairs';
+import random from '../random';
+import game from '..';
+
+const rules = 'Find the greatest common divisor of given numbers.';
+
+const findfcd = (number1, number2) => {
+  while (number1 !== 0 && number2 !== 0) {
+    if (number1 > number2) {
+      return findfcd(number2, number1 % number2);
+    } return findfcd(number1, number2 % number1);
+  } return number1 + number2;
+};
+
+const question = () => {
+  const randomNumber1 = random(1, 100);
+  const randomNumber2 = random(1, 100);
+  return cons(`${randomNumber1} ${randomNumber2}`, `${findfcd(randomNumber1, randomNumber2)}`);
+};
+
+export default () => game(question, rules);
